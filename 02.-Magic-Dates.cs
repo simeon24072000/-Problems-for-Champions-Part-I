106db86e58d33a7fac69b23322086947376223d4
SimeonCholakov <65452815+simeon24072000@users.noreply.github.com>
2020-07-06 23:05:24 +0300

    Update 02.-Magic-Dates.cs

--- 02.-Magic-Dates.cs
+++ 02.-Magic-Dates.cs
@@ -1,237 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace array.example
+namespace ChangeTiles
 {
     class ChangeTiles
     {
+        static StringBuilder YearDigits = new StringBuilder();
         static void Main()
         {
             int StartYear = int.Parse(Console.ReadLine());
             int EndYear = int.Parse(Console.ReadLine());
             int Weigth = int.Parse(Console.ReadLine());
-            //DateTime start = DateTime.Now;
+
             int year = 1900;
-            StringBuilder YearDigits = new StringBuilder();
-            int length = 0;
-            string YeardigitsStr = "";
-            int Sumofdigits = 0;
             var counter = 0;
-            var isMore = false;
 
             for (int years = StartYear; years <= EndYear; years++)
             {
                 for (int months = 1; months <= 12; months++)
                 {
-                    if (months == 4 || months == 6 || months == 9 || months == 11 )
+                    if (months == 4 || months == 6 || months == 9 || months == 11)
                     {
-                        for (int days = 1; days <= 30; days++)
-                        {
-                            YearDigits.Append(days);
-                            YearDigits.Append(months);
-                            YearDigits.Append(years);
-                            YeardigitsStr = YearDigits.ToString();
-                            length = YeardigitsStr.Length;
-                            for (int i = 0; i < length; i++)
-                            {
-                                for (int j = i + 1; j < length; j++)
-                                {
-                                    if (Sumofdigits > Weigth)
-                                    {
-                                        isMore = true;
-                                        break;
-                                    }
-                                    else Sumofdigits += (YeardigitsStr[i] - '0') * (YeardigitsStr[j] - '0');
-                                }
-                                if (isMore)
-                                {
-                                    break;
-                                }
-                            }
-                            if (Sumofdigits == Weigth && (years >= StartYear && years <= EndYear))
-                            {
-                                counter++;
-                                if (days >= 10)
-                                {
-                                    if (months >= 10)
-                                    {
-                                        Console.WriteLine("{0}-{1}-{2}", days, months, years);
-                                    }
-                                    else Console.WriteLine("{0}-0{1}-{2}", days, months, years);
-                                }
-                                else if(days < 10)
-                                {
-                                    if (months < 10)
-                                    {
-                                        Console.WriteLine("0{0}-0{1}-{2}", days, months, years);
-                                    }
-                                    else Console.WriteLine("0{0}-{1}-{2}", days, months, years);
-                                }
-
-                            }
-                            YearDigits.Clear();
-                            Sumofdigits = 0;
-                            isMore = false;
-                        }
+                        SumMonthDigits(StartYear, EndYear, Weigth, ref counter, years, months, 30);
                     }
                     else if (months == 2)
                     {
                         if (year == years)
                         {
-                            for (int days = 1; days <= 29; days++)
-                            {
-                                YearDigits.Append(days);
-                                YearDigits.Append(months);
-                                YearDigits.Append(years);
-                                YeardigitsStr = YearDigits.ToString();
-                                length = YeardigitsStr.Length;
-                                for (int i = 0; i < length; i++)
-                                {
-                                    for (int j = i + 1; j < length; j++)
-                                    {
-                                        if (Sumofdigits > Weigth)
-                                        {
-                                            isMore = true;
-                                            break;
-                                        }
-                                        else Sumofdigits += (YeardigitsStr[i] - '0') * (YeardigitsStr[j] - '0');
-                                    }
-                                    if (isMore)
-                                    {
-                                        break;
-                                    }
-                                }
-                                if (Sumofdigits == Weigth && (years >= StartYear && years <= EndYear))
-                                {
-                                    counter++;
-                                    if (days >= 10)
-                                    {
-                                        if (months >= 10)
-                                        {
-                                            Console.WriteLine("{0}-{1}-{2}", days, months, years);
-                                        }
-                                        else Console.WriteLine("{0}-0{1}-{2}", days, months, years);
-                                    }
-                                    else if (days < 10)
-                                    {
-                                        if (months < 10)
-                                        {
-                                            Console.WriteLine("0{0}-0{1}-{2}", days, months, years);
-                                        }
-                                        else Console.WriteLine("0{0}-{1}-{2}", days, months, years);
-                                    }
-                                }
-                                YearDigits.Clear();
-                                Sumofdigits = 0;
-                                isMore = false;
-                            }
+                            SumMonthDigits(StartYear, EndYear, Weigth, ref counter, years, months, 29);
                             year += 4;
                         }
                         else
                         {
-                            for (int days = 1; days <= 28; days++)
-                            {
-                                YearDigits.Append(days);
-                                YearDigits.Append(months);
-                                YearDigits.Append(years);
-                                YeardigitsStr = YearDigits.ToString();
-                                length = YeardigitsStr.Length;
-                                for (int i = 0; i < length; i++)
-                                {
-                                    for (int j = i + 1; j < length; j++)
-                                    {
-                                        if (Sumofdigits > Weigth)
-                                        {
-                                            isMore = true;
-                                            break;
-                                        }
-                                        else Sumofdigits += (YeardigitsStr[i] - '0') * (YeardigitsStr[j] - '0');
-                                    }
-                                    if (isMore)
-                                    {
-                                        break;
-                                    }
-                                }
-                                if (Sumofdigits == Weigth && (years >= StartYear && years <= EndYear))
-                                {
-                                    counter++;
-                                    if (days >= 10)
-                                    {
-                                        if (months >= 10)
-                                        {
-                                            Console.WriteLine("{0}-{1}-{2}", days, months, years);
-                                        }
-                                        else Console.WriteLine("{0}-0{1}-{2}", days, months, years);
-                                    }
-                                    else if (days < 10)
-                                    {
-                                        if (months < 10)
-                                        {
-                                            Console.WriteLine("0{0}-0{1}-{2}", days, months, years);
-                                        }
-                                        else Console.WriteLine("0{0}-{1}-{2}", days, months, years);
-                                    }
-                                }
-                                YearDigits.Clear();
-                                Sumofdigits = 0;
-                                isMore = false;
-                            }
+                            SumMonthDigits(StartYear, EndYear, Weigth, ref counter, years, months, 28);
                         }
                     }
                     else
                     {
-                        for (int days = 1; days <= 31; days++)
-                        {
-                            YearDigits.Append(days);
-                            YearDigits.Append(months);
-                            YearDigits.Append(years);
-                            YeardigitsStr = YearDigits.ToString();
-                            length = YeardigitsStr.Length;
-                            for (int i = 0; i < length; i++)
-                            {
-                                for (int j = i + 1; j < length; j++)
-                                {
-                                    if (Sumofdigits > Weigth)
-                                    {
-                                        isMore = true;
-                                        break;
-                                    }
-                                    else Sumofdigits += (YeardigitsStr[i] - '0') * (YeardigitsStr[j] - '0');
-                                }
-                                if (isMore)
-                                {
-                                    break;
-                                }
-                            }
-                            if (Sumofdigits == Weigth && (years >= StartYear && years <= EndYear))
-                            {
-                                counter++;
-                                if (days >= 10)
-                                {
-                                    if (months >= 10)
-                                    {
-                                        Console.WriteLine("{0}-{1}-{2}", days, months, years);
-                                    }
-                                    else Console.WriteLine("{0}-0{1}-{2}", days, months, years);
-                                }
-                                else if (days < 10)
-                                {
-                                    if (months < 10)
-                                    {
-                                        Console.WriteLine("0{0}-0{1}-{2}", days, months, years);
-                                    }
-                                    else Console.WriteLine("0{0}-{1}-{2}", days, months, years);
-                                }
-                            }
-                            YearDigits.Clear();
-                            Sumofdigits = 0;
-                            isMore = false;
-                        }
+                        SumMonthDigits(StartYear, EndYear, Weigth, ref counter, years, months, 31);
                     }
                 }
             }
@@ -239,8 +45,57 @@ namespace array.example
             {
                 Console.WriteLine("No");
             }
-            //DateTime end = DateTime.Now;
-            //Console.WriteLine(end - start);
+        }
+
+        private static void SumMonthDigits(int StartYear, int EndYear, int Weigth,
+            ref int counter, int years, int months, int endDay)
+        {
+            for (int days = 1; days <= endDay; days++)
+            {
+                int Sumofdigits = 0;
+
+                YearDigits.Append(days);
+                YearDigits.Append(months);
+                YearDigits.Append(years);
+
+                string YearDigitsStr = YearDigits.ToString();
+                int length = YearDigitsStr.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = i + 1; j < length; j++)
+                    {
+                        if (Sumofdigits > Weigth)
+                        {
+                            goto WeigthNotMatched;
+                        }
+                        else Sumofdigits += (YearDigitsStr[i] - '0') * (YearDigitsStr[j] - '0');
+                    }
+                }
+                if (Sumofdigits == Weigth && years >= StartYear && years <= EndYear)
+                {
+                    counter++;
+                    if (days >= 10)
+                    {
+                        if (months >= 10)
+                        {
+                            Console.WriteLine("{0}-{1}-{2}", days, months, years);
+                        }
+                        else Console.WriteLine("{0}-0{1}-{2}", days, months, years);
+                    }
+                    else if (days < 10)
+                    {
+                        if (months < 10)
+                        {
+                            Console.WriteLine("0{0}-0{1}-{2}", days, months, years);
+                        }
+                        else Console.WriteLine("0{0}-{1}-{2}", days, months, years);
+                    }
+                }
+
+                WeigthNotMatched:;
+                YearDigits.Clear();
+            }
         }
     }
 }
